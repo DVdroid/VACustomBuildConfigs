4a74f5e6052b900c208da8d1a15f5d830a6fea1e
Vikash Anand <anandin02@gmail.com>
2020-04-09 18:13:29 +0530

    VACustomBuildConfigs: Initial Commit.

---
+++ VACustomBuildConfigs/bundle-config/Script.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+#  Script.sh
+#  VACustomBuildConfigs
+#
+#  Created by Vikash Anand on 09/04/20.
+#  Copyright © 2020 Vikash Anand. All rights reserved.
+
+cp -fp "${SRCROOT}/VACustomBuildConfigs/Resources/${XCC_CONFIG_PATH}" "${SRCROOT}/VACustomBuildConfigs/Generated/Config/Config.plist"
